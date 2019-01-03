ec42e1c3cc6601ad2f709dc64385d01df42233c9
Bas van Dijk <v.dijk.bas@gmail.com>
2019-01-03 01:18:57 +0100

    Be more verbose when building the libfoo C++ library

--- libfoo/default.nix
+++ libfoo/default.nix
@@ -4,4 +4,9 @@ stdenv.mkDerivation rec {
   version = "1.0.0";
   src = ./.;
   nativeBuildInputs = [ cmake ];
+
+  cmakeFlags = [
+    # Be more verbose so we can see the exact compiler and linker commands.
+    "-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON"
+  ];
 }
